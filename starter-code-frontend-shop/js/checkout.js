35fa4ad377e02352fb5eb330b32cb7bd0ec0395d
josep <josep_1000@hotmail.com>
2025-11-02 20:02:58 +0100

    S2.2 Ejercicio 6 - validación formulario

--- starter-code-frontend-shop/js/checkout.js
+++ starter-code-frontend-shop/js/checkout.js
@@ -1,28 +1,57 @@
 
 // Exercise 6
+
 const validate = () => {
-	let error = 0;
-	// Get the input fields
 	const fName = document.getElementById("fName");
 	const fEmail = document.getElementById("fEmail");
-
-	// Get the error elements
-	const errorName = document.getElementById("errorName");
-	const errorEmail = document.getElementById("errorEmail");  
+	const fAddress = document.getElementById("fAddress");
+	const fLastN = document.getElementById("fLastN");
+	const fPassw = document.getElementById("fPassword");
+	const ftelef = document.getElementById("fPhone");
 	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value.trim() == ""){
-		error++;
-	}
+	const campos = [
+		{input: fName, regex: /^[A-Za-z]+$/},
+		{input: fEmail, regex: /^\S+@\S+\.\S+$/},
+		{input: fLastN, regex: /^[A-Za-z]+$/},
+		{input: fPassw, regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/},
+		{input: ftelef, regex: /^\d+$/}
+	]
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Please fill in all required fields.");
-	}else{
-		alert("Form submitted successfully");
-	}
+	const miFormulario = document.getElementById('form');
+	
+	miFormulario.addEventListener('submit', (event) => {
+			event.preventDefault();
+
+			let hayError = false;
+
+			campos.forEach(({ input, regex}) => {
+				const valor = input.value.trim();
+
+				if (valor.length < parseInt(input.getAttribute('minlength'))) {
+					input.classList.add("is-invalid");
+					hayError = true;
+					return;
+				}
+
+				if (!regex.test(valor)) {
+					input.classList.add("is-invalid");
+					hayError = true;
+				} else{
+					input.classList.remove("is-invalid");
+				}
+			});
+			
+			if(fAddress.value.trim().length < parseInt(fAddress.getAttribute('minlength'))){
+				fAddress.classList.add("is-invalid");
+				hayError = true;
+			}else {
+				fAddress.classList.remove("is-invalid");
+			}
+
+			if (!hayError) {
+				miFormulario.submit(); 
+			}
+	});
+	
 
 }
